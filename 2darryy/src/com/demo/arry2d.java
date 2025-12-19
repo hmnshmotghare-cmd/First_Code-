bb79318fd85f4b5a1901ae15d6448ae2791f6eb5
DELL <DELL@DESKTOP-OSH1VIH>
2025-12-19 15:56:15 +0530

    2D Array in Java

---
+++ 2darryy/src/com/demo/arry2d.java
@@ -0,0 +1,141 @@
+package com.demo;
+import java.util.Scanner;
+
+public class arry2d {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a[][] = new int[3][5];
+		
+		
+//		class i/p:
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<5;j++) {
+				System.out.println("Please enter the marks of class student " +""+ i+ " " + j);
+	     		a[i][j]=sc.nextInt();
+				
+			}
+			
+		}
+		
+		
+//		class 0 I/P
+//		hard code
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][0]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][1]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][2]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][3]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][4]=sc.nextInt();
+//		class 0 I/P
+//		Hard code
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][0]=11;
+//		a[0][1]=12;
+//		a[0][2]=13;
+//		a[0][3]=14;
+//		a[0][4]=15;
+		
+//		class 1 I/P
+//		hard code
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][0]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][1]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][2]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][3]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][4]=sc.nextInt();
+//		hard code
+//		a[1][0]=21;
+//		a[1][1]=22;
+//		a[1][2]=23;
+//		a[1][3]=24;
+//		a[1][4]=25;
+		
+		
+//		hard code
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][0]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][1]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][2]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][3]=sc.nextInt();
+//		System.out.println("Please enter the marks of class 0 student 0");
+//		a[0][4]=sc.nextInt();
+//		hard code
+//		class 2 I/P
+//		a[2][0]=31;
+//		a[2][1]=32;
+//		a[2][2]=33;
+//		a[2][3]=34;
+//		a[2][4]=35;
+
+		
+		
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<5;j++) {
+				System.out.print(a[i][j]+" ");
+				
+			}
+			System.out.println();
+			
+		}
+		
+//		hard code
+//		for(int i=0;i<3;i++) {
+//			System.out.print(a[i][0]+" ");
+//			System.out.print(a[i][1]+" ");
+//			System.out.print(a[i][2]+" ");
+//			System.out.print(a[i][3]+" ");
+//			System.out.print(a[i][4]+" ");
+//			System.out.println();
+//			
+//		}
+		
+		
+		
+//		//print
+////		class 0 O/P
+////		hard code
+//		System.out.print(a[0][0]+" ");
+//		System.out.print(a[0][1]+" ");
+//		System.out.print(a[0][2]+" ");
+//		System.out.print(a[0][3]+" ");
+//		System.out.print(a[0][4]+" ");
+//		System.out.println();
+//		
+//		//		class 1 O/P
+////		hard code
+//		System.out.print(a[1][0]+" ");
+//		System.out.print(a[1][1]+" ");
+//		System.out.print(a[1][2]+" ");
+//		System.out.print(a[1][3]+" ");
+//		System.out.print(a[1][4]+" ");
+//		System.out.println();
+//		
+//		
+//		//		class 2 O/P
+////		hard code
+//		System.out.print(a[2][0]+" ");
+//		System.out.print(a[2][1]+" ");
+//		System.out.print(a[2][2]+" ");
+//		System.out.print(a[2][3]+" ");
+//		System.out.print(a[2][4]+" ");
+//		System.out.println();
+		
+		
+		
+
+	}
+
+}
